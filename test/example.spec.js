68e2893e403996a2b3936038bf5eb4620964138d
Alex Gill <alex.gill@theuniprogroup.com>
2020-08-06 18:13:32 +0100

    Update example.spec.js

--- test/example.spec.js
+++ test/example.spec.js
@@ -1,7 +1,7 @@
 import { add } from '../src/add'
 
-describe('A suite', () => {
-  it('works', () => {
-    expect(true).toBe(true)
+describe('Add function', () => {
+  it('should be able to calculate sum of 5 and 7', () => {
+    expect(add(5, 7)).toEqual(12)
   })
 })
